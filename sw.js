e6f455d675de4354e1f93addaf04148a16d2b427
sukhadj <sukhadj1998@gmail.com>
2026-09-24 17:17:46 -0400

    add bell sound

--- sw.js
+++ sw.js
@@ -1,14 +1,12 @@
 // Offline cache for Bag HIIT. Bump VERSION whenever you change any file.
-const VERSION = 'bag-hiit-v3';
+const VERSION = 'bag-hiit-v5';
 const CORE = ['./', './index.html', './manifest.webmanifest',
-  './icons/icon-180.png', './icons/icon-192.png', './icons/icon-512.png'];
-const CLIPS = ['comingup','cool','cross','crossb','done','go','guard','half','jab','jabb','lastround','lhook','lhookb','lover','lupper','lupperb','moderate','move','n1','n1b','n2','n2b','n3','n3b','n4','n4b','n5','n5b','n6','n6b','next','r1','r10','r2','r3','r4','r5','r6','r7','r8','r9','ready','rest','restgood','resume','rhook','rhookb','roll','roundbreak','rover','rupper','rupperb','set1','set2','set3','set4','set5','setfinal','slipl','slipr','stepin','stepout','ten','test','then','thenmod','thirtyleft','w_bob_left','w_bob_right','w_cover','w_footwork','w_freestyle','w_movement','w_shadow','w_slipping','w_step_left','w_step_right'];
-const AUDIO = ['heart','michael'].flatMap(v => CLIPS.map(k => `./audio/${v}/${k}.mp3`));
+  './icons/icon-180.png', './icons/icon-192.png', './icons/icon-512.png',
+  './audio/heart.json', './audio/michael.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(VERSION).then(async c => {
     await c.addAll(CORE);
-    await Promise.all(AUDIO.map(u => c.add(u).catch(() => {})));
   }).then(() => self.skipWaiting()));
 });
 self.addEventListener('activate', e => {
